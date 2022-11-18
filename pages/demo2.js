915db58806dc41db78d4fc4ce8e5e8e096d61176
blitzve0 <frustasio@gmail.com>
2022-11-18 16:29:01 +0530

    laptop less

--- pages/demo2.js
+++ pages/demo2.js
@@ -7,11 +7,17 @@ export default function Home() {
   //& View
   return (
     <div className={"layout"}>
+      {/* //$ Temporary Background */}
       <img src="/bg2.png" className="backdrop" alt="bg" />
+      <img src="/mobilebg2.png" className="backdrop-mobile" alt="bg" />
+
+      {/* //$ Modal View */}
       <div className="modal2">
         <div className="heading-big">Fill More Information</div>
-        <div style={{ width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
-          <div style={{ display: "flex", flexDirection: "column" }}>
+
+        {/* //@ Input Fields */}
+        <div className="modal2-row">
+          <div className="modal2-row1">
             <InputField name="Username" value="Harsh" />
             <InputField name="Last Name" value="Dangi" />
             <InputField name="Apartment Suite No. etc" value="Texas" />
@@ -19,7 +25,7 @@ export default function Home() {
             <InputField name="DOB" value="20-12-2022" type="date" id="birthday" />
             <InputField name="Phone No" value="408-456-1245" />
           </div>
-          <div style={{ display: "flex", flexDirection: "column" }}>
+          <div className="modal2-row1">
             <InputField name="Email" value="abc@gmail.com" />
             <InputField name="Street Name" value="Street No 2" />
             <DropDown name="City" value={["USA", "Canada", "Australia", "London"]} />
@@ -28,31 +34,27 @@ export default function Home() {
             <InputField name="Additional Notes" value="" />
           </div>
         </div>
-        <div style={{ display: "flex", justifyContent: "flex-end", alignItems: "center", marginTop: "2vw", marginRight: "7vw" }}>
-          <div onClick={() => alert("Booking Done")} style={{ width: "11.56vw", height: "2.06vw", cursor: "pointer", }}>
-            <div style={{ width: "11.56vw", height: "2.06vw", paddingTop: "0.5vw", paddingBottom: "0.5vw", paddingLeft: "2.56vw", paddingRight: "2.18vw", backgroundColor: '#E8BFAF', borderRadius: 22, borderWidth: 1, borderStyle: "solid", display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', }}>
-              <div style={{ width: "11.56vw", height: "0.875vw", fontSize: "1vw", fontWeight: '600', color: 'black', textTransform: 'uppercase', }}>BOOK SLOT</div>
-              <div style={{ width: "0.625vw", }} />
-              <img style={{ width: "0.5vw", height: "0.5vw", }} src="/arrow.png" />
-            </div>
-          </div>
-        </div>
-        <div style={{ display: "flex", justifyContent: "flex-end", alignItems: "center", margin: "2vw", marginTop: "-1.5vw" }}>
-          <div style={{ textDecoration: "underline", fontWeight: "600", fontSize: "1.15vw" }}>Clear</div>
+
+        {/* //@ Book Slot Button */}
+        <Button click={() => alert("Booking Done")} label="BOOK SLOT" />
+
+        {/* //@ Clear Fields */}
+        <div className="clear-fields">
+          <div className="clear-text">Clear</div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
 //$ Dropdown Component
 function DropDown({ name, value }) {
   return (
-    <div style={{ marginTop: "2vw", position: "relative", width: "21.875vw", height: "4.06vw", display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-end', }}>
-      <div style={{ fontSize: "1.125vw", fontWeight: '300', color: 'black', }}>Select {name}</div>
-      <div style={{ height: "0.25vw", }} />
-      <img style={{ width: "1vw", height: "1vw", borderRadius: "0.5vw", position: "absolute", right: "1vw", top: "2.5vw", pointerEvents: "none" }} src="/icon.png" />
-      <select style={{ width: "21.875vw", height: "2.5vw", paddingTop: "0.75vw", paddingBottom: "0.5vw", paddingLeft: "1vw", paddingRight: "1vw", backgroundColor: 'white', borderRadius: "0.5vw", borderStyle: 'solid', borderWidth: 1, borderStyle: 'solid', borderColor: 'black', display: 'inline-flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', }} name="cars" id="cars">
+    <div className="drop2">
+      <div className="drop2-label">Select {name}</div>
+      <div className="drop2-break" />
+      <img className="drop2-icon" src="/icon.png" />
+      <select className="drop2-select" name="cars" id="cars">
         <option value="volvo">{value[0]}</option>
         <option value="saab">{value[1]}</option>
         <option value="mercedes">{value[2]}</option>
@@ -65,11 +67,26 @@ function DropDown({ name, value }) {
 //$ Input Component
 function InputField({ name, value, type }) {
   return (
-    <div style={{ marginTop: "2vw", width: "21.875vw", height: "4.06vw", display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-end', }}>
-      <p style={{ width: "auto", height: "0.97vw", fontSize: "1.125vw", fontWeight: '300', color: 'rgba(50, 50, 77, 1)', }}>{name}</p>
-      <div style={{ height: "0.58vw", }} />
-      <div style={{ width: "21.875vw", height: "2.5vw", paddingTop: "0.75vw", paddingBottom: "0.5vw", paddingLeft: "1vw", paddingRight: "1vw", backgroundColor: 'white', borderRadius: "0.5vw", borderStyle: 'solid', borderWidth: 1, borderStyle: 'solid', borderColor: 'black', display: 'inline-flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', }}>
-        <input type={type} style={{ width: "21.875vw", height: "1.25vw", fontSize: "1vw", color: 'rgba(0, 0, 0, 0.85)', border: "none" }} placeholder={value} />
+    <div className="textinput">
+      <p className="textinput-label">{name}</p>
+      <div className="textinput-break" />
+      <div className="textinput-row" >
+        <input type={type} className="textinput-input" placeholder={value} />
+      </div>
+    </div>
+  )
+}
+
+//$ Button Component
+function Button({ click, label }) {
+  return (
+    <div className="button2" >
+      <div onClick={click} className="button2-click" >
+        <div className="button2-row" >
+          <div className="button2-label" >{label}</div>
+          <div className="button2-break" />
+          <img className="button2-icon" src="/arrow.png" />
+        </div>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,72 @@
-import Router from "next/router";
+import Router from 'next/router'
 /// Imports
 
 //& Page 1
 export default function Home() {
-
   //& View
   return (
-    <div className={"layout"}>
-
+    <div className={'layout'}>
       {/* //$ Temporary Background */}
       <img src="/bg.png" className="backdrop" alt="bg" />
+      <img src="/mobilebg.png" className="backdrop-mobile" alt="bg" />
 
       {/* //$ Modal View */}
       <div className="modal1">
         <div className="close-button">X</div>
         <div className="heading-big">Book Appointment</div>
-        <div className="desc-small">Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, </div>
+        <div className="desc-small">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula
+          consectetur,{' '}
+        </div>
         {/* //@ Dropdowns */}
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <DropDown name="Location" value={["USA", "Canada", "Australia", "London"]} />
-          <DropDown name="Service" value={["Test Kit", "Order", "Report", "Other"]} />
-          <DropDown name="Test Menu" value={["Option 1", "Option 2", "Option 3", "Option 4"]} />
+        <div className="selector">
+          <DropDown name="Location" value={['USA', 'Canada', 'Australia', 'London']} />
+          <DropDown name="Service" value={['Test Kit', 'Order', 'Report', 'Other']} />
+          <DropDown name="Test Menu" value={['Option 1', 'Option 2', 'Option 3', 'Option 4']} />
         </div>
         {/* //@ Button */}
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "2vw" }}>
-          <div onClick={() => Router.push("/demo2")} style={{ width: "10.3vw", height: "2.06vw", cursor: "pointer", }}>
-            <div style={{ width: "10.3vw", height: "2.06vw", paddingTop: "0.5vw", paddingBottom: "0.5vw", paddingLeft: "2.56vw", paddingRight: "2.18vw", backgroundColor: 'rgba(238, 129, 85, 1)', borderRadius: 22, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', }}>
-              <div style={{ width: "5.75vw", height: "0.875vw", fontSize: "1vw", fontWeight: '600', color: 'black', textTransform: 'uppercase', }}>proceed</div>
-              <div style={{ width: "0.625vw", }} />
-              <img style={{ width: "0.5vw", height: "0.5vw", }} src="/arrow.png" />
-            </div>
-          </div></div>
+        <Button click={() => Router.push('/demo2')} label="proceed" />
       </div>
     </div>
   )
 }
 
-
 //$ Dropdown Component
 function DropDown({ name, value }) {
   return (
-    <div style={{ paddingLeft: "3vw", marginTop: "2vw", position: "relative", width: "15.18vw", height: "3.75vw", display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-end', }}>
-      <div style={{ fontSize: "1.125vw", fontWeight: '300', color: 'black', }}>Select {name}</div>
-      <div style={{ height: "0.25vw", }} />
-      <img style={{ width: "1vw", height: "1vw", borderRadius: "0.5vw", position: "absolute", right: "-2vw", top: "2.15vw", pointerEvents: "none" }} src="/icon.png" />
-      <select style={{ width: "15.18vw", paddingTop: "0.625vw", paddingBottom: "0.625vw", paddingLeft: "1vw", paddingRight: "0.75vw", backgroundColor: 'white', borderRadius: "0.5vw", borderStyle: 'solid', borderWidth: 1, borderStyle: 'solid', borderColor: 'black', display: 'inline-flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', }} name="cars" id="cars">
+    <div
+      className="drop"
+    >
+      <div className="drop-label">Select {name}</div>
+      <div className="drop-break" />
+      <img className="drop-icon" src="/icon.png" />
+      <select
+        className="drop-select"
+        name="cars"
+        id="cars"
+      >
         <option value="volvo">{value[0]}</option>
         <option value="saab">{value[1]}</option>
         <option value="mercedes">{value[2]}</option>
         <option value="audi">{value[3]}</option>
       </select>
+    </div>
+  )
+}
+
+//$ Button
+function Button({ click, label }) {
+  return (
+    <div className="clicker">
+      <div onClick={click} className="clicker-button">
+        <div
+          className="clicker-text"
+        >
+          <div className="clicker-label">{label}</div>
+          <div className="clicker-break" />
+          <img className="clicker-arrow" src="/arrow.png" />
+        </div>
+      </div>
     </div>
   )
 }
